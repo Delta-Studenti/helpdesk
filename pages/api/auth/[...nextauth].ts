5f58e5d76e2a04697a9ec1bd98ab5ae61ba20ae0
Karel Koudelka <k.koudelka01@gmail.com>
2021-04-14 19:41:16 +0200

    fix: auth roles

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,11 +6,25 @@ export default NextAuth({
     Providers.IdentityServer4({
       id: "identity-server4",
       name: "Delta - Identity Server",
-      scope: "openid profile name email",
+      scope: "openid profile name email roles",
       params: { grant_type: "authorization_code" },
       domain: "identita.delta-skola.cz",
       clientId: "helpdesk",
       clientSecret: process.env.NEXT_PUBLIC_IS_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    jwt: async (token, user, account, profile, isNewUser) => {
+      if (profile) {
+        token.profile = profile;
+      }
+      return Promise.resolve(token);
+    },
+    session: async (session, user) => {
+      return Promise.resolve({
+        ...session,
+        user: { ...user },
+      });
+    },
+  },
 });
