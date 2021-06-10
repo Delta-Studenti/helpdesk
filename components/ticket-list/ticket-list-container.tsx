bf640121471939d0856c152645a9ce7c368b2b42
Kryštof <krystof.kratky2003@gmail.com>
2021-06-10 22:39:15 +0200

    barely functional

--- components/ticket-list/ticket-list-container.tsx
+++ components/ticket-list/ticket-list-container.tsx
@@ -1,8 +1,6 @@
 import Grid from "@material-ui/core/Grid";
 import { styled } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
-import { compose, spacing, palette } from "@material-ui/system";
-import Paper from "@material-ui/core/Paper";
+import { Container, Button, Typography } from "@material-ui/core";
 import React, { useState } from "react";
 import { useTicketsQuery } from "../../src/graphql/tickets.graphql";
 import { create } from "node:domain";
@@ -14,57 +12,46 @@ const TicketListContainer: React.FC = () => {
   if (error) return <div>error</div>;
   if (loading) return <div>loading</div>;
 
-  const Tag = styled(Button)({
-    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-    border: 0,
-    borderRadius: 3,
-    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-    color: "white",
-    height: 24,
-    width: "auto",
-    padding: "0 30px",
-  });
-
   const tickets = data.tickets.map(
     ({ id, title, author, status, created, ticketTags, ticketMessages }) => {
       const createdAt = new Date();
       console.log(new Date(created));
       return (
-        <div item container direction="column" xs={12} md={8} key={id}>
-          <h1>{title}</h1>
-          <div>
+        <Container key={id}>
+          <Typography
+            variant="h4"
+            align="left"
+            color="textPrimary"
+            gutterBottom={false}
+          >
+            {title}
+          </Typography>
+          <Typography align="left" color="textSecondary" gutterBottom={true}>
             {author.name} · {createdAt.getTime()} ·{" "}
             {status === 0
               ? "nezahájeno"
               : status === 2
               ? "hotovo"
               : "pracuje se na tom"}
-          </div>
-          <div>{ticketMessages[0].messages}</div>
+          </Typography>
+          <Typography gutterBottom={true}>
+            {ticketMessages[0].messages}
+          </Typography>
           <div>
             {ticketTags.map(({ id, title }) => {
-              return <Tag key={id}>{title}</Tag>;
+              return (
+                <Button key={id} variant="contained" color="secondary">
+                  {title}
+                </Button>
+              );
             })}
           </div>
-        </div>
+        </Container>
       );
     },
   );
 
-  return (
-    <Grid container>
-      <Grid item container direction="column" xs={12} md={4}></Grid>
-      <Grid item container direction="column" xs={12} md={8}></Grid>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      {tickets}
-    </Grid>
-  );
+  return <div>{tickets}</div>;
 };
 
 export default TicketListContainer;
